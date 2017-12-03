f7219fad263a758f9c4d269f0197fdda9db9b726
Ameya Rao <ameyarao98@gmail.com>
2017-12-03 15:30:44 +0100

    Menu bar hover

--- js.js
+++ js.js
@@ -1,11 +1,15 @@
 $(document).ready(() => {
   $('.login').on('click', () => {
     $('.login-ting').toggle();
+    $(event.currentTarget).toggleClass('navselected');
+    $('.signup').removeClass('navselected');
     $('.signup-ting').hide();
   });
   
   $('.signup').on('click', () => {
     $('.signup-ting').toggle();
+    $(event.currentTarget).toggleClass('navselected');
+    $('.login').removeClass('navselected');
     $('.login-ting').hide();
   });
   
