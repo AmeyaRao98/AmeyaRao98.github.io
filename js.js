d4a88b21406f5cffd77d5dbc0750bedec2327be6
Ameya Rao <ameyarao98@gmail.com>
2017-12-05 13:30:02 +0100

    info page

--- js.js
+++ js.js
@@ -1,23 +1,31 @@
 $(document).ready(() => {
+
+  $('.movielist').fadeIn(1300);
+  setTimeout(function(){
+       $('.titl').addClass('vis');
+   }, 1000);
+
   $('.login').on('click', () => {
     $('.login-ting').slideToggle('fast');
     $(event.currentTarget).toggleClass('navselected');
     $('.signup').removeClass('navselected');
     $('.signup-ting').slideUp('fast');
   });
-  
+
   $('.signup').on('click', () => {
     $('.signup-ting').slideToggle('fast');
     $(event.currentTarget).toggleClass('navselected');
     $('.login').removeClass('navselected');
     $('.login-ting').slideUp('fast');
   });
-  
-  $('.main').fadeIn(1300);
-  
-  setTimeout(function(){
-       $('.titl').addClass('vis');
-   }, 1000);
-  
-  
-    }); 
+
+  $('.box').on('click', () => {
+    $('.info').slideDown('fast');
+    $('.main').hide();
+  });
+
+  $('#back').on('click', () => {
+    $('.info').slideUp('fast');
+    $('.main').show();
+  });
+    });
